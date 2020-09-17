fd456b54381994b6b3814262269f5f96b6da00aa
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-09-17 14:05:10 -0400

    Create Card.vue

---
+++ components/index/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card" style="background-image: ">
+    <a href="props.link">
+      <div class="gradientOverlay">
+        <h3 class="articleTitle" alt="">
+          {props.headline}
+        </h3>
+
+        <article class="mtAuto}">
+          <h3 alt="">
+            {props.type}
+          </h3>
+          <h3 alt="">
+            {props.date}
+          </h3>
+        </article>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style>
+@import "~/assets/styles/card.css";
+</style>
