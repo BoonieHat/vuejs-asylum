f4b95e9244190abd1f61803d7f6629e01e3fd646
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-09-17 14:05:18 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <Header />
-    <IndexBanner />
+    <Banner />
+    <Content />
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
-import IndexBanner from '~/components/IndexBanner.vue'
+import Banner from '~/components/index/Banner.vue'
+import Content from '~/components/index/Content.vue'
 export default {
   components: {
     Header,
-    IndexBanner
+    Banner,
+    Content
   }
 }
 </script>
