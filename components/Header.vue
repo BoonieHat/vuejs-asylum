a8d1c1317d8f446172941973e4cdf127d85da3db
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-09-17 13:15:15 -0400

    inital

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <div class="headerContainer">
+      <div class="headerLogo">
+        <a href="/">
+          <img src="../assets/images/logo.png" style="width: 150px"></img>
+        </a>
+      </div>
+
+      <button class="buttonHeader">
+        <a href="/play">
+          <a>PLAY NOW</a>
+        </a>
+      </button>
+
+      <button class="buttonHeader">
+        <a href="/store">STORE</a>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss">
+@import "../assets/styles/header.scss";
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+@import "../assets/styles/theme.css";
+@import "../assets/styles/home.css";
+@import "../assets/styles/main.css";
+
+html {
+  font-family: Roboto, 'Helvetica Neue', Arial, sans-serif
+}
+
+body {
+  margin: 0px;
+}
+
+h1, h2, h3, h4, h5 {
+  margin: 0;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
