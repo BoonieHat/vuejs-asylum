da5cee63b5742ac05c4c3212d9ed14d8040aff1f
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-09-17 14:05:05 -0400

    Create Footer.vue

---
+++ components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <div class="headerContainer">
+      <div class="headerLogo">
+        <a href="/">
+          <img src="../assets/images/logowhite.png" style="width: 150px"></img>
+        </a>
+      </div>
+
+      <button class="buttonHeader">
+        <a href="/play">PLAY NOW</a>
+      </button>
+
+      <button class="buttonHeader">
+        <a href="/store">STORE</a>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+@import "../assets/styles/footer.css";
+</style>
