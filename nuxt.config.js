a3602a3cdef65eecbb070fcdafb928b5832603b4
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-10-08 16:01:04 -0400

    wip

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   target: 'server',
   head: {
-    title: 'MyAsylum',
+    title: 'Asylum',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
