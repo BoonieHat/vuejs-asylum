2c55e1348763599523053ee7caa8fb73e54dea20
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-10-08 09:03:06 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,12 @@
-
+export default {
+  head: {
+    title: "My Nuxt.js Application!"
+  }
+};
+/*
 export default {
   buildDir: 'nuxt-dist',
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
   target: 'server',
-
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
   head: {
     title: 'MyAsylum',
     meta: [
@@ -22,54 +18,24 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
-  /*
-  ** Global CSS
-  */
   css: [
   ],
-
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
   plugins: [
   ],
-
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
   components: true,
-
-  /*
-  ** Nuxt.js dev-modules
-  */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     //'@nuxtjs/eslint-module'
   ],
-
-  /*
-  ** Nuxt.js modules
-  */
   modules: [
     // Doc: https://http.nuxtjs.org
     '@nuxt/http'
   ],
-
-  /*
-  ** Server Middleware
-  */
   serverMiddleware: {
     '/api': '~/api'
   },
-
-  /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
   build: {
     analyze: false
   }
 }
+*/
