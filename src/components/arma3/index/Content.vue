d2b05c224162c3a6fb24ee7b79efdb2e3ea8eb6e
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-10-23 07:29:07 -0400

    wip

--- src/components/arma3/index/Content.vue
+++ src/components/arma3/index/Content.vue
@@ -2,49 +2,65 @@
   <div class="content-root minecraft-content">
     <div class="content-root-next">
       <ArmaSection 
-        brightness="70%" 
-        opacity="0.15" 
-        imageUrl="./section1.jpg" 
+        brightness="100%" 
+        opacity="0.25" 
+        imageUrl="./arma3/section1.jpg" 
         cardIcon="fas fa-envelope" 
         cardTitle="Join Gangs" 
         cardText="Want to work with the criminal underworld and take over the island? Join a gang."
       />
       <ArmaSection 
-        brightness="70%" 
-        opacity="0.15" 
-        imageUrl="./section2.jpg" 
+        brightness="100%" 
+        opacity="0.25" 
+        imageUrl="./arma3/section1.jpg" 
+        cardIcon="fas fa-envelope" 
+        cardTitle="Join Fire Deptartment" 
+        cardText="Want to work with the criminal underworld and take over the island? Join a gang."
+      />
+      <ArmaSection 
+        brightness="100%" 
+        opacity="0.25" 
+        imageUrl="./arma3/section1.jpg" 
+        cardIcon="fas fa-envelope" 
+        cardTitle="Join Police Deptartment" 
+        cardText="Want to work with the criminal underworld and take over the island? Join a gang."
+      />
+      <ArmaSection 
+        brightness="100%" 
+        opacity="0.25" 
+        imageUrl="./arma3/section2.jpg" 
         cardIcon="fas fa-wallet" 
         cardTitle="Control Cartels" 
         cardText="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
       />
       <ArmaSection 
-        brightness="70%" 
-        opacity="0.15" 
-        imageUrl="./section3.jpg" 
+        brightness="100%" 
+        opacity="0.25" 
+        imageUrl="./arma3/section3.jpg" 
         cardIcon="fas fa-question" 
         cardTitle="Bank Heists" 
         cardText="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
       />
       <ArmaSection 
-        brightness="70%" 
-        opacity="0.15" 
-        imageUrl="./section4.jpg" 
+        brightness="100%" 
+        opacity="0.25" 
+        imageUrl="./arma3/section4.jpg" 
         cardIcon="fas fa-layer-group" 
         cardTitle="Prison Breaks" 
         cardText="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
       />
       <ArmaSection 
-        brightness="70%" 
-        opacity="0.15" 
-        imageUrl="./section5.jpg" 
+        brightness="100%" 
+        opacity="0.25" 
+        imageUrl="./arma3/section5.jpg" 
         cardIcon="fas fa-gem" 
         cardTitle="Federal Reserve Robbery" 
         cardText="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
       />
       <ArmaSection 
-        brightness="70%" 
-        opacity="0.15" 
-        imageUrl="./section5.jpg" 
+        brightness="100%" 
+        opacity="0.25" 
+        imageUrl="./arma3/section6.jpg" 
         cardIcon="fas fa-gem" 
         cardTitle="Raid Evidence Lockup" 
         cardText="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
